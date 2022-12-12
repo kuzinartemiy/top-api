53668975b913e027748e4df923c4f00a9b3eb10d
Artemiy Kuzin <kuzin_artemiy@list.ru>
2022-12-12 20:53:23 +0500

    feat: add static serve with '/static/' prefix

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+// import { ServeStaticModule } from '@nestjs/serve-static';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 
 @Module({
+  // imports: [ServeStaticModule],
   controllers: [FilesController],
   providers: [FilesService],
 })
